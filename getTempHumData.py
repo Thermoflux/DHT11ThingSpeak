5f50eadfc7b5ac98751769134dcf83406d98e2c3
Thermoflux <akotha05@gmail.com>
2017-03-23 13:00:48 -0500

    - Updated indentation

--- getTempHumData.py
+++ getTempHumData.py
@@ -39,10 +39,8 @@ fErrRec = pwd + 'TempHumErrorStatus.log'
 
 def getSensorData():
     RHW, TW = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHTpin)
-    
     #Convert from Celius to Farenheit
     TWF = 9/5*TW+32
-   
     # return dict
     return (str(RHW), str(TW),str(TWF))
 
@@ -53,44 +51,24 @@ def main():
 
     baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI
     print baseURL
-    
+
     while True:
-        
+	
 		try:
-			
-	   		 # Log data to file
-            		mData = "%s,%s,%s,%s\n" %(mTime,TW,TWF,RHW)
+			mTime = time.strftime("%d/%m/%Y %H:%M:%S",time.gmtime())
+			RHW, TW, TWF = getSensorData()
+
+			# Log data to file
+        		mData = "%s,%s,%s,%s\n" %(mTime,TW,TWF,RHW)
             		logfile = open(fname,'a',0)
             		logfile.write(mData)
-            		logfile.close()
-            
-	    	      # Log error status to file
+	            	logfile.close()
+          
+	    		# Log error status to file
             		logfile = open(fErrRec,'w',0)
             		logfile.write("1\n")
             		logfile.close()
-			try: 
-				
-            			mTime = time.strftime("%d/%m/%Y %H:%M:%S", time.gmtime())
-            			RHW, TW, TWF = getSensorData()
-
-            			# Send data to ThingSpeak takes some time to open the connection
-            			f = urllib2.urlopen(baseURL + 
-                                	"&field1=%s&field2=%s&field3=%s" % (TW, TWF, RHW)+
-                                	"&field4=%s" % ("1"))
-
-            			print f.read()
-            			print TW + " " + TWF+ " " + RHW + " " + "1"
-            			f.close()
-			except:
-				
-				# Log error status to file
-			        logfile = open(fErrRec,'w',0)
-    	        		logfile.write("0")
-        			logfile.close()
-            			print 'Failed to connect to the server.'
-
-            
-		      time.sleep(int(myDelay))
+			time.sleep(int(myDelay))
 
         	except:
             		# Log error status to file
@@ -100,6 +78,22 @@ def main():
             		print 'Exception caught'
             		# break  		  Dont want to exit everytime we loose network connection.
 
+		try:
+        		# Send data to ThingSpeak takes some time to open the connection
+        		f = urllib2.urlopen(baseURL +                               "&field1=%s&field2=%s&field3=%s" % (TW, TWF, RHW)+ "&field4=%s" % ("1"))
+       			print f.read()
+    			print TW + " " + TWF+ " " + RHW + " " + "1"
+			f.close()
+		except:
+			
+			# Log error status to file
+            		logfile = open(fErrRec,'w',0)
+        		logfile.write("0")
+       	    		logfile.close()
+       			print 'Failed to connect to the server.'
+
+            
+			
 # call main
 if __name__ == '__main__':
     main()
